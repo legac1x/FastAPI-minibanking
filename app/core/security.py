75cd918e308e0a4457b797ce2b702eacaadd95d1
Zakhar Shashev <zaharschaschev@yandex.ru>
2025-09-17 21:18:35 +0300

    Add logs in security

--- app/core/security.py
+++ app/core/security.py
@@ -12,6 +12,7 @@ from app.api.schemas.users import UserOut
 from app.db.database import get_db
 from app.db.models import User
 from app.core.config import settings
+from app.core.logging import logger
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/users/login")
 ACCESS_TOKEN_EXPIRE_MINUTES = 15
@@ -21,62 +22,85 @@ ALGORITHM = settings.ALGORITHM
 pwd = CryptContext(schemes=['bcrypt'], deprecated="auto")
 
 async def get_user_from_db(username: str, session: AsyncSession) -> User:
+    logger.info("Получение пользователя из БД")
     query = await session.execute(select(User).where(User.username == username))
     user = query.scalar_one_or_none()
     if not user:
+        logger.warning("Пользователь '%s' не существует", username)
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail="User wasn't found."
         )
+    logger.debug("Возврат экземпляр User пользователя '%s'", username)
     return user
 
 def get_hashed_password(password: str):
+    logger.info("Хеширование пароля")
     return pwd.hash(password)
 
 def verify_password(user_password: str, hash_password: str):
+    logger.info("Сравнение введенного пароля с хешем")
     return pwd.verify(user_password, hash_password)
 
 async def authenticate_user(username: str, password: str, session: AsyncSession) -> User:
+    logger.info("Аутентификация пользователя")
     user = await get_user_from_db(username, session)
     if not verify_password(password, user.hashed_password):
+        logger.warning("Некорректное имя '%s' или пароль", username)
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Invalid username or password."
         )
     if not user.is_email_verified:
+        logger.warning("Пользователь '%s' не верифицирован", username)
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Email not verified. Check your email."
         )
+    logger.debug("Возврат экземпляра User пользователя '%s'", username)
     return user
 
 async def create_access_token(data: dict, expire_time: timedelta | None = None) -> str:
+    logger.info("Создание access токена")
     to_encode = data.copy()
+    logger.debug("Установка времени истечения токена: %s", f"{expire_time} через аргумент" if expire_time else "по умолчанию")
     if expire_time:
         expire = datetime.now(timezone.utc) + expire_time
     else:
         expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
+    logger.debug("Генерируем JWT токен")
     access_token = jwt.encode(payload=to_encode, key=SECRET_KEY, algorithm=ALGORITHM)
     return access_token
 
 async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)], db: Annotated[AsyncSession, Depends(get_db)]) -> UserOut:
     try:
+        logger.info("Аутентификация пользователя через JWT токен")
+        logger.debug("Получение полезной нагрузки")
         payload = jwt.decode(jwt=token, key=SECRET_KEY, algorithms=[ALGORITHM])
         username = payload.get('sub')
         if not username:
+            logger.warning("Не найден username токена")
             raise HTTPException(
                 status_code=status.HTTP_401_UNAUTHORIZED,
                 detail="Invalid token"
             )
         user = await get_user_from_db(username, db)
+        logger.info("Успешная аутентификация пользователя '%s'", username)
         return UserOut(
             first_name=user.first_name,
             last_name=user.last_name,
             username=user.username
         )
     except jwt.exceptions.InvalidTokenError:
+        logger.warning("JWT токен некорректен")
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Invalid token"
+        )
+    except jwt.ExpiredSignatureError:
+        logger.warning("JWT токен просрочен")
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Token expired"
         )
